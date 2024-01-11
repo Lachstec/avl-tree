c3ccd965d5978ce39eb30dd594fe122382d9d975
Leon Lux <leonlux@icloud.com>
2024-01-11 21:34:54 +0100

    rewrite tree to use NonNull and unsafe rust

--- src/avl_tree.rs
+++ src/avl_tree.rs
@@ -1,177 +1,87 @@
-#![allow(unused)]
-use std::cmp::{Ordering, max};
-use std::iter::FromIterator;
+#![allow(dead_code)]
+use std::{ptr::NonNull, marker::PhantomData};
+use std::cmp::{max, Ordering};
 
-type AvlTreeRef<T> = Option<Box<AvlTreeNode<T>>>;
+type Link<T> = Option<NonNull<AvlTreeNode<T>>>;
 
 #[derive(Debug, Clone, PartialEq)]
-struct AvlTreeNode<T: Ord> {
+pub struct AvlTreeNode<T: Ord> {
     pub data: T,
-    left: AvlTreeRef<T>,
-    right: AvlTreeRef<T>,
-    height: usize,
+    pub left: Link<T>,
+    pub right: Link<T>,
+    pub parent: Link<T>,
+    pub height: usize,
 }
 
 impl<T: Ord> AvlTreeNode<T> {
-    pub fn new(value: T) -> Self {
-        Self {
-            data: value,
-            left: None,
-            right: None,
-            height: 0,
-        }
+    pub fn update_height(&mut self) {
+        let left_height = self.left.as_ref().map_or(0, |left| unsafe { (*left.as_ptr()).height });
+        let right_height = self.right.as_ref().map_or(0, |right| unsafe { (*right.as_ptr()).height });
+
+        self.height = 1 + max(left_height, right_height);
     }
 
-    pub fn set_left(&mut self, node: AvlTreeNode<T>) {
-        self.left = Some(Box::new(node));
-    }
+    pub fn balance_factor(&mut self) -> usize {
+        let left_height = self.left
+            .as_ref()
+            .map_or(0, |left| unsafe { (*left.as_ptr()).height });
+        let right_height = self.right
+            .as_ref()
+            .map_or(0, |right| unsafe { (*right.as_ptr()).height });
 
-    pub fn set_right(&mut self, node: AvlTreeNode<T>) {
-        self.right = Some(Box::new(node));
-    }
-
-    fn update_height(&mut self) {
-        self.height = 1 + max(self.left_height(), self.right_height());
-    }
-
-    fn left_height(&self) -> usize {
-        self.left.as_ref().map_or(0, |left| left.height)
-    }
-
-    fn right_height(&self) -> usize {
-        self.left.as_ref().map_or(0, |right| right.height)
+        right_height - left_height
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct AvlTree<T: Ord> {
-    root: AvlTreeRef<T>,
+    root: Link<T>,
+    _marker: PhantomData<T>,
 }
 
 impl<T: Ord> AvlTree<T> {
     pub fn new() -> Self {
         Self {
-            root: None
+            root: None,
+            _marker: PhantomData,
         }
     }
 
-    pub fn insert(&mut self, value: T) {
-        let mut previous_nodes = Vec::<*mut AvlTreeNode<T>>::new();
-        let mut current_subtree = &mut self.root;
-
-        while let Some(node) = current_subtree {
-            previous_nodes.push(&mut **node);
-            match node.data.cmp(&value) {
-                Ordering::Greater => current_subtree = &mut node.left,
-                Ordering::Equal => return,
-                Ordering::Less => current_subtree = &mut node.right,
-            }
-        }
-        *current_subtree = Some(Box::new(AvlTreeNode::new(value)));
-        for node_ptr in previous_nodes.into_iter().rev() {
-            let node = unsafe { &mut *node_ptr };
-            node.update_height();
-        }
-    }
-}
-
-impl<'a, T: 'a + Ord> AvlTree<T> {
-    fn iter(&'a self) -> AvlTreeIterator<'a, T> {
-        AvlTreeIterator {
-            prev_nodes: Vec::new(),
-            current_tree: &self.root,
-        }
-    }
-}
-
-pub struct AvlTreeIterator<'a, T: Ord> {
-    prev_nodes: Vec<&'a AvlTreeNode<T>>,
-    current_tree: &'a AvlTreeRef<T>
-}
-
-impl<'a, T: 'a + Ord> Iterator for AvlTreeIterator<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match *self.current_tree {
-                None => match self.prev_nodes.pop() {
-                    None => return None,
-                    Some(ref prev_node) => {
-                        self.current_tree = &prev_node.right;
-                        return Some(&prev_node.data)
-                    }
-                },
-                Some(ref current_node) => {
-                    if current_node.left.is_some() {
-                        self.prev_nodes.push(&current_node);
-                        self.current_tree = &current_node.left;
-
-                        continue;
-                    }
-
-                    if current_node.right.is_some() {
-                        self.current_tree = &current_node.right;
-                        return Some(&current_node.data)
-                    }
-
-                    self.current_tree = &None;
-                    return Some(&current_node.data)
+    pub fn insert(&mut self, data: T) -> bool {
+        unsafe {
+            let mut current_node = &mut self.root;
+            while let Some(node_ptr) = current_node {
+                let node = &mut (*node_ptr.as_ptr());
+                match node.data.cmp(&data){
+                    Ordering::Greater => current_node = &mut node.left,
+                    Ordering::Equal => return false,
+                    Ordering::Less => current_node = &mut node.right,
                 }
             }
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(AvlTreeNode {
+                data,
+                left: None,
+                right: None,
+                parent: None,
+                height: 0
+            })));
+            //TODO: Set parent as needed.
+            *current_node = Some(new);
+            true
         }
-    }
-}
-
-impl<T: Ord> FromIterator<T> for AvlTree<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-        let mut tree = Self::new();
-
-        for elem in iter {
-            tree.insert(elem);
-        }
-
-        tree
     }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
-    fn insert() {
+    fn simple_insert() {
         let mut tree: AvlTree<i32> = AvlTree::new();
-        tree.insert(22);
-        tree.insert(22);
-        tree.insert(12);
-
-        assert_eq!(
-            tree.root,
-            Some(Box::new(AvlTreeNode {
-                data: 22,
-                left: Some(Box::new(AvlTreeNode {
-                    data: 12,
-                    left: None,
-                    right: None,
-                    height: 0,
-                })),
-                right: None,
-                height: 1
-            }))
-        )
-    }
-
-    #[test]
-    fn iterator() {
-        let mut tree: AvlTree<u32> = AvlTree::new();
-        tree.insert(3);
-        tree.insert(2);
-        tree.insert(1);
-
-        let mut iter = tree.iter();
-        assert_eq!(iter.next(), Some(&1));
-        assert_eq!(iter.next(), Some(&2));
-        assert_eq!(iter.next(), Some(&3));
+        assert!(tree.insert(1));
+        assert!(!tree.insert(1));
+        assert!(tree.insert(2));
+        // TODO: Test if tree contains elements as expected
     }
 }
