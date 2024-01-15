f921e49d3ca04b5c30cd71f6be489b6258cff252
leonlux <leonlux@icloud.com>
2024-01-15 10:43:50 +0100

    add some basic cli args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use clap::Parser;
+use std::path;
+
 mod avl_tree;
 
+/// Program to visualize AVL-Trees
+#[derive(Debug, Parser)]
+struct Args {
+    /// Print intermediate Trees
+    #[arg(short = 'i')]
+    intermediates: bool,
+    /// Output directory. Defaults to current working directory.
+    #[arg(short = 'o')]
+    output_directory: Option<path::PathBuf>,
+    /// Values to put into the Tree.
+    #[arg(short = 'v')]
+    values: Vec<i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Intermediates: {}", args.intermediates);
 }
